6c46d3b46c58e04c8403d4d94d8f6c032cccd1c9
hiz-boyd <gideonj666@gmail.com>
2025-07-14 17:02:18 -0700

    Fix: SECTION C

--- src/Component/SectionC.jsx
+++ src/Component/SectionC.jsx
@@ -52,7 +52,7 @@ function SectionC() {
 
           <div className="mt-8 md:mt-12 flex justify-center">
             <Link
-              to="/scholarshiplisting"
+              to="/scholarship"
               className="animate-search-pulse transform rounded-lg bg-[#0000FF] px-8 md:px-12 py-3 md:py-4 font-semibold text-base md:text-lg text-white transition-all duration-300 hover:scale-105 hover:animate-none hover:bg-blue-700 active:scale-95"
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
